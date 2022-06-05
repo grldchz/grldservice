8cad3eafbad3db39983be7feea12096168396a9a
Nathan Rudisill <nrudisill@threatconnect.com>
2022-06-05 06:10:52 -0400

    Updated license header

--- auth/service.php
+++ auth/service.php
@@ -4,12 +4,12 @@
 
 	Copyright (C) 2022 grilledcheeseoftheday.com
 
-    GRLDCHZ is free software: you can redistribute it and/or modify
+    grilledcheeseoftheday.com is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    GRLDCHZ is distributed in the hope that it will be useful,
+    grilledcheeseoftheday.com is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
